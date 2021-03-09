639a8ee3cd84fd5213310d9daa21c08cec9ce948
errk01 <erik.taylor30@gmail.com>
2021-03-09 14:52:42 -0700

    Updated code

---
+++ e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/model/UserDetails.java
@@ -0,0 +1,10 @@
+package com.ecommerce.ecommerceTTS.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Transient;
+import java.util.Collection;
+
+public class UserDetails {
+
+}
