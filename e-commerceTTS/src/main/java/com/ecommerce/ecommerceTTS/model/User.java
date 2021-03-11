fede375db044df3d0925a57c2ab05a42a42b47be
errk01 <erik.taylor30@gmail.com>
2021-03-11 10:21:20 -0700

    Today's code with working login, logout and cart page

--- e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/model/User.java
+++ e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/model/User.java
@@ -36,6 +36,7 @@ public class User implements UserDetails {
     private boolean enabled = true;
     @Transient
     private Collection<GrantedAuthority> authorities = null;
+
     // this here allows the Id to be auto generated
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -53,9 +54,9 @@ public class User implements UserDetails {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+//    private Set<Role> roles;
 
 
     @ElementCollection
--- e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/service/UserService.java
+++ e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/service/UserService.java
@@ -6,6 +6,7 @@ import com.ecommerce.ecommerceTTS.model.Product;
 import com.ecommerce.ecommerceTTS.model.User;
 import com.ecommerce.ecommerceTTS.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,10 +25,12 @@ public class UserService implements UserDetailsService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User findByUsername(String username){
+
         return userRepository.findByUsername(username);
     }
     public void saveNew(User user){
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 
     public void saveExisting(User user){
